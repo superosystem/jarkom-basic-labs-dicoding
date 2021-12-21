7276ef5e71e324f4dabc84d60ffdf1ac0f1cd332
Syahril <gusrylmubarok@gmail.com>
2021-12-22 06:46:55 +0700

    Membuat Configuration Authentication without Login

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/filter/JwtRequestFilter.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/filter/JwtRequestFilter.java
@@ -1,2 +1,19 @@
-package com.gusrylmubarok.sinaukoding.hris.filter;public class JwtRequestFilter {
+package com.gusrylmubarok.sinaukoding.hris.filter;
+
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 }
