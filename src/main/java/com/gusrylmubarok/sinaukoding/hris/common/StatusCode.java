73f7e48bc2f2fa8eb31c9aef523b9bd865187dc9
Syahril <gusrylmubarok@gmail.com>
2021-12-22 03:08:48 +0700

    Membuat Rest Result dan Status Code

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/common/StatusCode.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/common/StatusCode.java
@@ -1,2 +1,16 @@
-package com.gusrylmubarok.sinaukoding.hris.common;public class StatusCode {
+package com.gusrylmubarok.sinaukoding.hris.common;
+
+public interface StatusCode {
+
+    String SAVE_SUCCESS = "0010";
+    String SAVE_FAILED = "0011";
+
+    String UPDATE_SUCCESS = "0012";
+    String UPDATE_FAILED = "0013";
+
+    String DELETE_SUCCESS = "0014";
+    String DELETE_FAILED = "0015";
+
+    String OPERATION_SUCCESS = "1112";
+    String OPERATION_FAILED = "1113";
 }
