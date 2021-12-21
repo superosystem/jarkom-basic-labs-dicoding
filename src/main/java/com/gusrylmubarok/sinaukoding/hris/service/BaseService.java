de4d4381e9a94aaa0bd2eae94025537598a32694
Syahril <gusrylmubarok@gmail.com>
2021-12-22 03:07:52 +0700

    Membuat BaseService

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/service/BaseService.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/service/BaseService.java
@@ -1,2 +1,46 @@
-package com.gusrylmubarok.sinaukoding.hris.service;public class BaseService {
+package com.gusrylmubarok.sinaukoding.hris.service;
+
+import com.gusrylmubarok.sinaukoding.hris.dao.BaseDAO;
+import com.gusrylmubarok.sinaukoding.hris.entity.BaseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+public abstract class BaseService<T extends BaseEntity<T>> {
+
+    protected abstract BaseDAO<T> getDAO();
+
+    @Transactional
+    public T save(T entity) {
+        return getDAO().save(entity);
+    }
+
+    @Transactional
+    public T update(T entity) {
+        if (entity.getId() != null) {
+            return getDAO().update(entity);
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public boolean delete(Long id) {
+        return getDAO().delete(getDAO().findReference(id)) != null;
+    }
+
+    @Transactional(readOnly = true)
+    public Long count(T param) {
+        return getDAO().count(param);
+    }
+
+    @Transactional(readOnly = true)
+    public T findOne(T param) {
+        return getDAO().findOne(param);
+    }
+
+    public Collection<T> find(T param, int offset, int limit) {
+        return getDAO().find(param, offset, limit);
+    }
+
 }
