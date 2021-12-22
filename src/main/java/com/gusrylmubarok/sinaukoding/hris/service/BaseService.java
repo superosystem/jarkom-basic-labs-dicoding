478395e0c1cc03409015f02bc66bc5973c3cfd57
Syahril <gusrylmubarok@gmail.com>
2021-12-22 16:35:58 +0700

    Make Base Service and Division Service

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/service/BaseService.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/service/BaseService.java
@@ -1,2 +1,47 @@
-package com.gusrylmubarok.sinaukoding.hris.service;public class BaseService {
+package com.gusrylmubarok.sinaukoding.hris.service;
+
+import com.gusrylmubarok.sinaukoding.hris.dao.BaseDAO;
+import com.gusrylmubarok.sinaukoding.hris.entity.BaseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+public abstract class BaseService<T extends BaseEntity<T>> {
+
+    protected abstract BaseDAO<T> getDAO();
+
+    @Transactional(readOnly = true)
+    public T findOne(T param){
+        return getDAO().findOne(param);
+    }
+
+    @Transactional(readOnly = true)
+    public Collection<T> find(T param, int offset, int limit){
+        return getDAO().find(param, offset, limit);
+    }
+
+    @Transactional(readOnly = true)
+    public Long count(T param){
+        return getDAO().count(param);
+    }
+
+    @Transactional
+    public T save(T entity){
+        return getDAO().save(entity);
+    }
+
+    @Transactional
+    public T update(T entity){
+        if (entity.getId() != null){
+            return getDAO().update(entity);
+        }
+        return null;
+    }
+
+    @Transactional
+    public boolean delete(Long id){
+        return getDAO().delete(getDAO().findReference(id)) != null;
+    }
+
+
 }
