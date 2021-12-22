f3eb9101d1694761e8d7f14f0daf724346b22db2
Syahril <gusrylmubarok@gmail.com>
2021-12-22 16:34:02 +0700

    Make BaseDAO and DivisionDAO

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/dao/DivisionDAO.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/dao/DivisionDAO.java
@@ -1,2 +1,19 @@
-package com.gusrylmubarok.sinaukoding.hris.dao;public class DivisionDAO {
+package com.gusrylmubarok.sinaukoding.hris.dao;
+
+import com.gusrylmubarok.sinaukoding.hris.entity.Division;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class DivisionDAO extends BaseDAO<Division> {
+    @Override
+    public List<Predicate> predicates(Division param, CriteriaBuilder builder, Root<Division> root, boolean isCount) {
+        List<Predicate> predicates = super.predicates(param, builder, root, isCount);
+
+        return predicates;
+    }
 }
