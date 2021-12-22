458e4a01a357ea921be2914df73e9b9e88023916
Syahril <gusrylmubarok@gmail.com>
2021-12-22 21:30:03 +0700

    Refactor library value jwt.secret

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/service/JwtTokenService.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/service/JwtTokenService.java
@@ -4,7 +4,7 @@ import com.gusrylmubarok.sinaukoding.hris.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
