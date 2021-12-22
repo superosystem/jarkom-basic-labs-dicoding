bbc655732048f9d59ea2116d42adadd0651a38d3
Syahril <gusrylmubarok@gmail.com>
2021-12-22 16:37:36 +0700

    Make Configuration Web Security without login

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/filter/JwtRequestFilter.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/filter/JwtRequestFilter.java
@@ -1,2 +1,18 @@
-package com.gusrylmubarok.sinaukoding.hris.filter;public class JwtRequestFilter {
+package com.gusrylmubarok.sinaukoding.hris.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
 }
