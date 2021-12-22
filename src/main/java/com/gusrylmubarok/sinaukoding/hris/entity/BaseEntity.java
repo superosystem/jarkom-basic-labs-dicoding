54ad129388c4b6d6d14942d8150fc52b6c77cd0a
Syahril <gusrylmubarok@gmail.com>
2021-12-22 16:33:14 +0700

    Make BaseEntity and Division Entity

--- src/main/java/com/gusrylmubarok/sinaukoding/hris/entity/BaseEntity.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/entity/BaseEntity.java
@@ -1,2 +1,73 @@
-package com.gusrylmubarok.sinaukoding.hris.entity;public class BaseEntity {
+package com.gusrylmubarok.sinaukoding.hris.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter @Setter
+@NoArgsConstructor
+@MappedSuperclass
+@DynamicUpdate
+@SuppressWarnings("unchecked")
+public abstract class BaseEntity<T> implements Serializable {
+
+    private static final long serialVersionUID = 2428620307308756675L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTime;
+
+    @Column(name = "deleted")
+    private Boolean deleted = Boolean.FALSE;
+
+    @Column(name = "deleted_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedTime;
+
+    @Column(name = "updated_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedTime;
+
+    @Column(name = "created_by", columnDefinition = "BIGINT(20)")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "deleted_by", columnDefinition = "BIGINT(20)")
+    @LastModifiedBy
+    private String deletedBy;
+
+    @Column(name = "updated_by", columnDefinition = "BIGINT(20)")
+    @LastModifiedBy
+    private String updatedBy;
+
+    @PrePersist
+    protected void onCreate(){
+        setCreatedTime(new Date());
+    }
+
+    @PreRemove
+    protected void onRemove(){
+        setDeleted(Boolean.TRUE);
+        setDeletedTime(new Date());
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        setUpdatedTime(new Date());
+    }
+
+
+
+
 }
--- src/main/java/com/gusrylmubarok/sinaukoding/hris/entity/Division.java
+++ src/main/java/com/gusrylmubarok/sinaukoding/hris/entity/Division.java
@@ -1,2 +1,24 @@
-package com.gusrylmubarok.sinaukoding.hris.entity;public class Division {
+package com.gusrylmubarok.sinaukoding.hris.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "division")
+@Getter @Setter
+@NoArgsConstructor
+public class Division extends BaseEntity<Division> {
+
+    private static final long serialVersionUID = 8910423892206400642L;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(255)")
+    private String name;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
 }
